4571aee781401c6120172c7dac50b1f559ed0a19
Mikhail Palazkou <polozkov1450@gmail.com>
2021-07-16 17:51:40 +0300

    task 4 first part

--- task_4/db.py
+++ task_4/db.py
@@ -2,7 +2,7 @@ import json
 
 
 def create_tables(connection, cursor):
-    create_student_table = "CREATE TABLE student(id int primary key," \
+    create_student_table = "create table student(id int primary key," \
                            "name varchar(40)," \
                            "birthday varchar(40)," \
                            "sex varchar(5));"
@@ -11,14 +11,14 @@ def create_tables(connection, cursor):
 
     print("Table student created successfully")
 
-    create_room_table = "CREATE TABLE room(id int primary key," \
+    create_room_table = "create table room(id int primary key," \
                         "name varchar(40));"
     cursor.execute(create_room_table)
     connection.commit()
 
     print("Table room created successfully")
 
-    create_relationship_table = "CREATE TABLE student_room(id int primary key," \
+    create_relationship_table = "create table student_room(id int primary key," \
                                 "student_id integer references student(id)," \
                                 "room_id integer references room(id));"
     cursor.execute(create_relationship_table)
@@ -46,20 +46,12 @@ def drop_tables(connection, cursor):
     return True
 
 
-def insert_data_into_tables(connection, cursor):
-
-    with open('students.json') as student_file:
-        student_data = json.load(student_file)
-
-    student_inset_values = str()
-
-    for student in student_data:
-        student_inset_values += f"({student['id']}, {student['name']}," \
-                                f"{student['birthday']}, {student['sex']}), "
-
-    cursor.execute(f"insert into student (id, name, birthday, sex) values {student_inset_values}")
-    connection.commit()
-    print('Successfully insert into table student')
+# def insert_data_into_tables(connection, cursor):
+#
+#     with open('students.json') as student_file:
+#         student_data = json.load(student_file)
+#
+#     print(student_data[0])
 
 
 def connection_close(connection, *args):
