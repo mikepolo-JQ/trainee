e50840875e866f01e283f4e7d1a93953c72d45cc
Mikhail Palazkou <polozkov1450@gmail.com>
2021-07-19 16:05:18 +0300

    switching to OOP

--- task_4/db.py
+++ task_4/db.py
@@ -1,196 +1,192 @@
 import json
 import time
+import pymysql
+import conf as settings
+
+sql_queries = {
+    'create_student': "create table student(id int primary key," \
+                      "name varchar(40)," \
+                      "birthday datetime, " \
+                      "sex varchar(2));",
+
+    'create_room': "create table room(id int primary key," \
+                   "name varchar(40));",
+
+    'create_student_room': "create table student_room(id serial primary key," \
+                           "student_id integer references student(id)," \
+                           "room_id integer references room(id));",
+
+    'get_room_name_and_count_of_students': "SELECT room.name, COUNT(student_room.student_id) as 'students'" \
+                                            " FROM room left JOIN student_room on student_room.room_id=room.id " \
+                                            "GROUP BY room.name",
+
+    'get_top5_young_rooms': "SELECT room.name, AVG(YEAR(CURRENT_DATE)-YEAR(student.birthday))" \
+                            " AS AverageAge FROM room INNER JOIN student_room " \
+                            "on room.id=student_room.room_id INNER JOIN student " \
+                            "on student_room.student_id=student.id " \
+                            "GROUP BY room.name ORDER BY AverageAge LIMIT 5",
+
+    'biggest_difference_in_the_age': "SELECT room.name, YEAR(MAX(student.birthday))-" \
+                                    "YEAR(MIN(student.birthday)) as Difference from room " \
+                                    "LEFT JOIN student_room on student_room.room_id=room.id " \
+                                    "LEFT JOIN student on student_room.student_id=student.id " \
+                                    "GROUP BY room.name ORDER BY Difference DESC LIMIT 5",
+
+    'difference_students_sex': "SELECT room.name FROM room LEFT JOIN student_room " \
+                    "ON student_room.room_id=room.id LEFT JOIN student " \
+                    "ON student.id=student_room.student_id GROUP BY room.name " \
+                    "HAVING MAX(student.sex)!=MIN(student.sex)"
+}
 
 
-def create_tables(connection, cursor):
-    create_student_table = "create table student(id int primary key," \
-                           "name varchar(40)," \
-                           "birthday datetime, " \
-                           "sex varchar(2));"
-    cursor.execute(create_student_table)
-    connection.commit()
+class DB:
+    def __init__(self):
+        # Connect to the database
+        connection = pymysql.connect(host=settings.HOST,
+                                     port=settings.PORT,
+                                     user=settings.USERNAME,
+                                     password=settings.PASSWORD,
+                                     database=settings.DATABASE_NAME,
+                                     cursorclass=pymysql.cursors.DictCursor)
 
-    print("Table student created successfully")
+        self.connection = connection
+        self.tables_names = ['student', 'room', 'student_room']
+        print("Successfully connected...")
 
-    create_room_table = "create table room(id int primary key," \
-                        "name varchar(40));"
-    cursor.execute(create_room_table)
-    connection.commit()
+    def __disconnect__(self):
+        self.connection.close()
+        print('Connection close...')
 
-    print("Table room created successfully")
+    def __execute_and_commit(self, sql_query: str):
+        with self.connection.cursor() as cursor:
+            cursor.execute(sql_query)
+            self.connection.commit()
 
-    create_relationship_table = "create table student_room(id serial primary key," \
-                                "student_id integer references student(id)," \
-                                "room_id integer references room(id));"
-    cursor.execute(create_relationship_table)
-    connection.commit()
+    def __fetchall(self, sql_query: str):
+        with self.connection.cursor() as cursor:
+            cursor.execute(sql_query)
+            rows = cursor.fetchall()
+            return rows
 
-    print("Table student_room created successfully")
+    # create tables
+    def create_table(self) -> bool:
+        # if not tables_names:
+        tables_names = self.tables_names
 
-    return True
+        for table_name in tables_names:
+            sql_query = sql_queries[f'create_{table_name}']
+            self.__execute_and_commit(sql_query)
 
+            print(f"Table {table_name} created successfully")
 
-def drop_student_table(connection, cursor, table_name):
-    cursor.execute(f"DROP TABLE {table_name};")
-    connection.commit()
-    print(f"Table {table_name} drop successfully")
+        return True
 
+    # drop tables
+    def drop_table(self) -> bool:
 
-def drop_tables(connection, cursor):
-    table_names = ['student', 'room', 'student_room']
+        # if not tables_names:
+        tables_names = self.tables_names
 
-    [drop_student_table(connection, cursor, table_name) for table_name in table_names]
-    return True
+        for table_name in tables_names:
+            self.__execute_and_commit(f"DROP TABLE {table_name};")
+            print(f"Table {table_name} drop successfully")
 
+        return True
 
-def insert_into_student_table(connection, cursor):
+    # insert data from student.json to the database
+    def insert_from_student_json(self):
 
-    with open('task_4/students.json') as student_file:
-        student_data = json.load(student_file)
+        with open('task_4/students.json') as student_file:
+            student_data = json.load(student_file)
 
-    values = str()
-    values_for_student_room = str()
+        values = str()
+        values_for_student_room = str()
 
-    start = time.time()
-    for student in student_data:
-        values += f"({student['id']}, '{student['name']}', '{student['birthday']}', '{student['sex']}')"
+        start = time.time()
+        for student in student_data:
+            values += f"({student['id']}, '{student['name']}', '{student['birthday']}', '{student['sex']}')"
 
-        values_for_student_room += f"({student['id']}, {student['room']})"
+            values_for_student_room += f"({student['id']}, {student['room']})"
 
-        if student['id'] != student_data[-1]['id']:
-            values += ', '
-            values_for_student_room += ', '
+            if student['id'] != student_data[-1]['id']:
+                values += ', '
+                values_for_student_room += ', '
 
-    cursor.execute("insert student(id, name, birthday, sex) values " + values + ';')
-    connection.commit()
+        self.__execute_and_commit("insert student(id, name, birthday, sex) values " + values + ';')
+        print('Insert in student successfully.')
 
-    cursor.execute("insert student_room(student_id, room_id) values " +
-                   values_for_student_room + ';')
-    connection.commit()
-    finish = time.time()
-    print(f"Insert student and student_room successfully! Total time: {finish - start:.2f}")
-    return True
+        self.__execute_and_commit("insert student_room(student_id, room_id) values " +
+                                  values_for_student_room + ';')
 
+        finish = time.time()
+        print(f"Insert in student_room successfully!\nTotal time: {finish - start:.2f}")
 
-def insert_into_room_table(connection, cursor):
+    # insert data from room.json to the database
+    def insert_from_room_json(self):
 
-    with open('task_4/rooms.json') as room_file:
-        room_data = json.load(room_file)
+        with open('task_4/rooms.json') as room_file:
+            room_data = json.load(room_file)
 
-    values = str()
+        values = str()
 
-    start = time.time()
-    for room in room_data:
-        values += f"({room['id']}, '{room['name']}')"
+        start = time.time()
+        for room in room_data:
+            values += f"({room['id']}, '{room['name']}')"
 
-        if room['id'] != room_data[-1]['id']:
-            values += ', '
+            if room['id'] != room_data[-1]['id']:
+                values += ', '
 
-    cursor.execute("insert room(id, name) values " + values + ';')
-    connection.commit()
+        self.__execute_and_commit("insert room(id, name) values " + values + ';')
 
-    finish = time.time()
+        finish = time.time()
+        print(f"Insert room successfully! Total time: {finish - start:.2f}")
 
-    print(f"Insert room successfully! Total time: {finish - start:.2f}")
+    def insert_data(self):
+        self.insert_from_student_json()
+        self.insert_from_room_json()
+        return True
 
-    return True
+    # database queries
+    def print_rooms_and_the_number_of_students(self):
+        sql_query = sql_queries['get_room_name_and_count_of_students']
 
+        rows = self.__fetchall(sql_query)
 
-def insert_data_into_tables(connection, cursor):
-    insert_into_student_table(connection, cursor)
-    insert_into_room_table(connection, cursor)
-    return True
+        print('\tROOM NAME\tSTUDENTS COUNT')
+        for row in rows:
+            print(f"\t{row['name']}\t{row['students']}")
 
+        return True
 
-# database queries
-def get_rooms_and_the_number_of_students(_connection, cursor):
-    sql_query = "SELECT room.name, COUNT(student_room.student_id) as 'students'" \
-                " FROM room left JOIN student_room on student_room.room_id=room.id " \
-                "GROUP BY room.name"
+    def print_top5_young_rooms(self):
+        sql_query = sql_queries['get_top5_young_rooms']
 
-    cursor.execute(sql_query)
-    rows = cursor.fetchall()
+        rows = self.__fetchall(sql_query)
 
-    print('\tROOM NAME\tSTUDENTS COUNT')
-    for row in rows:
-        print(f"\t{row['name']}\t{row['students']}")
+        print('\tROOM NAME\tAVERAGE AGE')
+        for row in rows:
+            print(f"\t{row['name']}\t{row['AverageAge']}")
 
-    return True
+        return True
 
+    def print_top5_rooms_with_the_biggest_difference_in_the_age(self):
+        sql_query = sql_queries['biggest_difference_in_the_age']
 
-def get_top5_young_rooms(_connection, cursor):
-    sql_query = "SELECT room.name, AVG(YEAR(CURRENT_DATE)-YEAR(student.birthday))" \
-                " AS AverageAge FROM room INNER JOIN student_room " \
-                "on room.id=student_room.room_id INNER JOIN student " \
-                "on student_room.student_id=student.id " \
-                "GROUP BY room.name ORDER BY AverageAge LIMIT 5"
+        rows = self.__fetchall(sql_query)
 
-    cursor.execute(sql_query)
-    rows = cursor.fetchall()
+        print('\tROOM NAME\tDIFFERENCE')
+        for row in rows:
+            print(f"\t{row['name']}\t{row['Difference']}")
 
-    print('\tROOM NAME\tAVERAGE AGE')
-    for row in rows:
-        print(f"\t{row['name']}\t{row['AverageAge']}")
+        return True
 
-    return True
+    def print_rooms_with_difference_students_sex(self):
+        sql_query = sql_queries['difference_students_sex']
 
+        rows = self.__fetchall(sql_query)
 
-def get_top5_biggest_difference_in_the_age_rooms(_connection, cursor):
-    sql_query = "SELECT room.name, YEAR(MAX(student.birthday))-" \
-                "YEAR(MIN(student.birthday)) as Difference from room " \
-                "LEFT JOIN student_room on student_room.room_id=room.id " \
-                "LEFT JOIN student on student_room.student_id=student.id " \
-                "GROUP BY room.name ORDER BY Difference DESC LIMIT 5"
+        print('\tROOM NAME')
+        for row in rows:
+            print(f"\t{row['name']}")
 
-    cursor.execute(sql_query)
-    rows = cursor.fetchall()
-
-    print('\tROOM NAME\tDIFFERENCE')
-    for row in rows:
-        print(f"\t{row['name']}\t{row['Difference']}")
-
-    return True
-
-
-def get_rooms_with_difference_students_sex(_connection, cursor):
-    sql_query = "SELECT room.name FROM room LEFT JOIN student_room " \
-                "ON student_room.room_id=room.id LEFT JOIN student " \
-                "ON student.id=student_room.student_id GROUP BY room.name " \
-                "HAVING MAX(student.sex)!=MIN(student.sex)"
-
-    cursor.execute(sql_query)
-    rows = cursor.fetchall()
-
-    print('\tROOM NAME')
-    for row in rows:
-        print(f"\t{row['name']}")
-
-    return True
-
-
-def clear(_connection, _cursor):
-    import os
-    os.system('clear||cls')
-    return True
-
-
-help_string = """
-create\t\tcreate tables student, rooms and student_room for m2m rel
-drop\t\tDrop all tables
-insert\t\tInsert data from json to database
-end\t\tTo exit from TASK_4
-clear\t\tClear terminal
-1\t\tsome-some-some
-2\t\tsome-some-some
-"""
-
-
-def print_command_list(_connection, _cursor):
-    print(help_string)
-    return True
-
-
-def connection_close(connection, *args):
-    connection.close()
-    print('Connection close...')
-    return False
+        return True
--- task_4/main.py
+++ task_4/main.py
@@ -1,54 +1,69 @@
-import pymysql
-import conf as settings
-import db
+from db import DB
+
+
+help_string = """
+List of all TASK_4 commands:
+?\t\tGet list of commands
+create\t\tCreate tables student, rooms and student_room for m2m rel
+drop\t\tDrop all tables
+insert\t\tEnter data from json file to the database
+end\t\tExit from TASK_4
+clear\t\tClear the terminal\n
+1\t\tGet list of rooms and the number of students in each of them
+2\t\tGet top 5 rooms with the smallest average age of students
+3\t\tGet top 5 rooms with the biggest difference in the age of students
+4\t\tGet list of rooms where students of different sexes live
+"""
+
+
+def print_command_list(_x):
+    print(help_string)
+    return True
+
+
+def clear(_x):
+    import os
+    os.system('clear||cls')
+    return True
+
 
 command_dict = {
-    'create': db.create_tables,
-    'drop': db.drop_tables,
-    'end': db.connection_close,
-    'insert': db.insert_data_into_tables,
-    'clear': db.clear,
-    '?': db.print_command_list,
+    'create': DB.create_table,
+    'drop': DB.drop_table,
+    'end': lambda _x: False,
+    'insert': DB.insert_data,
+    'clear': clear,
+    '?': print_command_list,
 
-    '1': db.get_rooms_and_the_number_of_students,
-    '2': db.get_top5_young_rooms,
-    '3': db.get_top5_biggest_difference_in_the_age_rooms,
-    '4': db.get_rooms_with_difference_students_sex
+    '1': DB.print_rooms_and_the_number_of_students,
+    '2': DB.print_top5_young_rooms,
+    '3': DB.print_top5_rooms_with_the_biggest_difference_in_the_age,
+    '4': DB.print_rooms_with_difference_students_sex
 }
 
 
 try:
     # Connect to the database
-    connection = pymysql.connect(host=settings.HOST,
-                                 port=settings.PORT,
-                                 user=settings.USERNAME,
-                                 password=settings.PASSWORD,
-                                 database=settings.DATABASE_NAME,
-                                 cursorclass=pymysql.cursors.DictCursor)
-
-    print("successfully connected...")
+    db_helper = DB()
 
     try:
-        with connection.cursor() as cursor:
-            # Create a new record
+        while True:
 
-            while True:
+            command = input("TASK_4 >>> ").lower()
 
-                command = input("TASK_4 >>> ").lower()
+            try:
+                handler = command_dict[command]
+            except KeyError as ex:
+                print("Bed request! Try '?' for see command list.")
+                continue
 
-                try:
-                    handler = command_dict[command]
-                except KeyError as ex:
-                    print("Bed request! Try '?' for see command list.")
-                    continue
+            result = handler(db_helper)
 
-                result = handler(connection, cursor)
-
-                if not result:
-                    break
+            if not result:
+                break
 
     finally:
-        connection.close()
+        db_helper.__disconnect__()
 
 
 except Exception as ex:
